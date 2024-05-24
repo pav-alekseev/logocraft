4f8a44ffcf27e7074a1536cde1e61a9b51601909
Karen Baratyants <karen.baratyants@vk.team>
2024-05-25 01:32:03 +0700

    added style input

--- src/components/StyleInput.svelte
+++ src/components/StyleInput.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
+  import CodeMirror from "svelte-codemirror-editor";
+  //@ts-ignore
+  import { oneDark } from "@codemirror/theme-one-dark";
+  import { css } from '@codemirror/lang-css';
 
   let styles: string = '';
   const dispatch = createEventDispatcher<{ submit: { styles: string } }>();
@@ -10,6 +14,7 @@
 </script>
 
 <div>
+  <CodeMirror lineWrapping={true} lang={css()} theme={oneDark} placeholder={'Введите стили'} useTab={true}></CodeMirror>
   <textarea bind:value={styles} placeholder="Введите дополнительные стили"></textarea>
   <button on:click={submitStyles}>Применить стили</button>
 </div>
