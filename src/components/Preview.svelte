8d808879e5f030e721da409a6777904d2c598f6e
Karen Baratyants <karen.baratyants@vk.team>
2024-05-25 11:39:21 +0700

    fix of updating

--- src/components/Preview.svelte
+++ src/components/Preview.svelte
@@ -5,15 +5,15 @@
   $: fullCode = htmlCode + (cssCode ? "<style>" + cssCode + "</style>" : "");
 </script>
 
-<iframe class="frame" title="preview" srcdoc={fullCode} style=""></iframe>
+{#key fullCode}
+  <iframe class="frame" title="preview" srcdoc={fullCode} style=""></iframe>
+{/key}
 
 <style>
   .frame {
     border-radius: var(--border-radius);
-
     width: 330px;
     height: 330px;
-
     border: none;
     background-color: #fff;
   }
